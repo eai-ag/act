4788a66858962b09328f273c64bccafc20001c3c
Max Polzin <m.polzin@embodiedai.ch>
2025-12-18 17:53:47 +0100

    fix: returning the number of valid steps to weight total metrics based on it

--- policy.py
+++ policy.py
@@ -31,6 +31,7 @@ class ACTPolicy(nn.Module):
             mask = (~is_pad.unsqueeze(-1)).float()
             l1 = (all_l1 * mask).sum() / mask.sum().clamp(min=1)
             loss_dict['l1'] = l1
+            loss_dict['num_valid'] = mask.sum().item()
             loss_dict['kl'] = total_kld[0]
             loss_dict['loss'] = loss_dict['l1'] + loss_dict['kl'] * self.kl_weight
             return loss_dict
